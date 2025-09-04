1a30df7dfbaef5da25347568d7b28dc74ae41545
Amanda Ellis <amanda.ellis002@student.swic.edu>
2025-09-04 15:45:47 -0500

    Add function to calculate letter grade

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ const GRADE_A_THRESHOLD = 90;
 const GRADE_B_THRESHOLD = 80;
 const GRADE_C_THRESHOLD = 70;
 const GRADE_D_THRESHOLD = 60;
+
+const calculateLetterGrade = (numericGrade) => {
+  if (numericGrade >= GRADE_A_THRESHOLD) {
+    return "A";
+  } else if (numericGrade >= GRADE_B_THRESHOLD) {
+    return "B";
+  } else if (numericGrade >= GRADE_C_THRESHOLD) {
+    return "C";
+  } else if (numericGrade >= GRADE_D_THRESHOLD) {
+    return "D";
+  }
+  return "F";
+};

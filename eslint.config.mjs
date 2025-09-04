d449de7219e73523222acdb902191bb89ebdad31
Amand Ellis <amanda.ellis002@student.swic.edu>
2025-09-04 16:17:35 -0500

    Remove no-magic-numbers rule from eslint config
    Removed 'no-magic-numbers' rule from ESLint configuration.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,14 +25,6 @@ export default [
       // Variables (Clean Code)
       "no-var": "error",
       "prefer-const": "error",
-      "no-magic-numbers": [
-        "warn",
-        {
-          ignore: [0, 1, -1],
-          ignoreArrayIndexes: true,
-          ignoreDefaultValues: true,
-        },
-      ],
 
       // Functions (Clean Code)
       "max-params": ["warn", ESLINT_THRESHOLDS.MAX_FUNCTION_PARAMS],
